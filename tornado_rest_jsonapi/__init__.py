8dc6144640cb92861fc74e7b7ededa8c36078cd6
Stefano Borini <sborini@enthought.com>
2017-05-30 14:19:49 +0100

    Exporting fundamentals at root package level

--- tornado_rest_jsonapi/__init__.py
+++ tornado_rest_jsonapi/__init__.py
@@ -1,1 +1,7 @@
 from .version import __version__ # noqa
+from .api import Api
+from .resource import Resource, ResourceList, ResourceDetails
+from .model_connector import ModelConnector
+from .exceptions import *
+
+from marshmallow_jsonapi import Schema, fields
