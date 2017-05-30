9f0a09807cec1a45b609358cb0500f31dd584c3e
Stefano Borini <sborini@enthought.com>
2017-05-30 14:22:15 +0100

    Added authenticator

--- tornado_rest_jsonapi/__init__.py
+++ tornado_rest_jsonapi/__init__.py
@@ -3,5 +3,6 @@ from .api import Api
 from .resource import Resource, ResourceList, ResourceDetails
 from .model_connector import ModelConnector
 from .exceptions import *
+from .authenticator import Authenticator, NullAuthenticator
 
 from marshmallow_jsonapi import Schema, fields
